78ac349a1a832e2b899bc6bf3d8c35209fddd3e3
mtoto <tszilagyi@outlook.com>
2017-08-29 22:50:01 +0200

    executable for pred gen

---
+++ callback.py
@@ -0,0 +1,14 @@
+import datetime
+import time
+
+def callback():
+    #now1=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    starttime=time.time()
+    if s == 0:
+        while True:
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            api_comms(now)
+            print(now)
+            time.sleep(300.0 - ((time.time() - starttime) % 300.0))
+            
+callback()
